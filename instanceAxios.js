ba656ec4069d0af7cc128e3579a1f9614d749dec
Rosssp <O.liGo.R@ya.ru>
2023-06-13 09:40:29 +0300

    temp http request pass

--- instanceAxios.js
+++ instanceAxios.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 import { setCookie, getCookie, deleteCookie, hasCookie } from "cookies-next";
+const https = require("https");
 
 const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API,
+    httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+    }),
     // withCredentials: true,
+    insecure: true,
     headers: {
         // Authorization: token?.replace("myspot_jwt2222=", ""),
         Authorization: getCookie("myspot_jwt2222"),
