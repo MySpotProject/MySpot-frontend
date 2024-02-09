d1124d1805fbe1359e5fd9787d5a4c1d1549c02f
Rosssp <O.liGo.R@ya.ru>
2024-02-09 15:57:01 +0300

    rework main page

--- constants/images.js
+++ constants/images.js
@@ -1,4 +1,6 @@
 import logo from "../assets/images/logo.svg";
+import logoMain from "../assets/images/logoMain.png";
+import logoMainTricks from "../assets/images/logoMainTricks.svg";
 import menu from "../assets/images/menu.svg";
 import bg from "../assets/images/BG.svg";
 import vk from "../assets/images/vk.svg";
@@ -7,10 +9,14 @@ import email from "../assets/images/email.svg";
 import railingLeft from "../assets/images/railings_left.svg";
 import railingRight from "../assets/images/railings_right.svg";
 import railings from "../assets/images/railings.svg";
+import railingss from "../assets/images/railingss.png";
 import star from "../assets/images/star.svg";
 
 export default {
     logo,
+    logoMainTricks,
+    logoMain,
+    railingss,
     menu,
     bg,
     vk,
